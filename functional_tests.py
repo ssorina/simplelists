e27188803819abdedd1b9a54dcc21fef5cfbb0ad
ssorina <sorina.negrila@3pillarglobal.com>
2015-01-05 15:19:20 +0200

    modified the first test case in the functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import unittest
 
 
@@ -14,6 +16,27 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn("To-Do", self.browser.title)
+        #find title on page h1 element
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        #find box necessary for writing input in the to-do list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        #write input into field and then save via Enter
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        #find saved input in table on page
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail("Finish the test!")
 
 
