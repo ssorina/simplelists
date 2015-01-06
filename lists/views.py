674d0a899adbfccffcc4b7ef927a4f9cc7ea4b8b
ssorina <sorina.negrila@3pillarglobal.com>
2015-01-06 13:06:06 +0200

    simplified unit tests, modified homepage template and homepage view

--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 from lists.models import Item
 #from lists.templates import home
 
 
 # Create your views here.
-def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
 
-    return render(request, 'home.html', {
-        'new_item_text': item.text,
-    })
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
