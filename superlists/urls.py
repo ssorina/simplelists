197c668637fc81f9ed5df19bdb45498bb4775dcf
ssorina <sorina.negrila@3pillarglobal.com>
2015-01-06 17:36:43 +0200

    modified views, their unit tests and added incremental url for new view

--- superlists/urls.py
+++ superlists/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/the-only-list-in-the-world/$',
+    url(r'^$', 'lists.views.home_page',
+        name='home'),
+    url(r'^lists/(.+)/$',
         'lists.views.view_list',
         name='view_list'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
